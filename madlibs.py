ec17830b0ebadc5a6144f49ec31dfe63372d2642
calipho <106757959+calipho@users.noreply.github.com>
2022-07-03 19:55:00 +0300

    python first class

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,26 @@
 def main():
-	# write your code here
+	time = input("Enter a time of day: ")
+	print(time) 
+	items = input("Enter a plural noun: ")
+	print(items)
+	name = input("Enter a name: ")
+	name = name.capitalize()
+	print(name)
+	sentence = input("Enter a sentence: ")
+	sentence = sentence.upper()
+	print(sentence)
+	verb = input("Enter a verb: ")
+	print(f"The time is {time} and the {items} are being {verb} by {name} .")
+#  items = dolls
+#  name = stephen sedoll
+#  example_dictionary = {"scream":"the future is made of dolls", "froze":"shake my head"}
 
+# name = name.capitalize()
+# x = scream.upper()
 
+# print(x)
+
+# print (name)
 
 if __name__ == '__main__':
 	main()
